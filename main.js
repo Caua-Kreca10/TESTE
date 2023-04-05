2b78feaaf4396acbd49eaa4f9116d5fda6afb138
Caua-Kreca10 <129890305+Caua-Kreca10@users.noreply.github.com>
2023-04-05 11:33:51 -0300

    alteração 2

--- main.js
+++ main.js
@@ -4,10 +4,11 @@ document.getElementById("botao").addEventListener("click",showname);
 
 function showcolor()
 {
-    document.body.style.backgroundColor= "red";
+    document.body.style.backgroundColor = "red";
 }
 function showname()
 {
     let usuario = document.getElementById("nome").value;
     document.getElementById("mensagem").innerText = "Meu nome é" + usuario;
+    document.getElementById("nome").value = "";
 }
