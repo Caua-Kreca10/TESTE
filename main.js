faf141d0afa1a5e9221f2d2e75c9323f5ee24a43
Caua-Kreca10 <129890305+Caua-Kreca10@users.noreply.github.com>
2023-04-05 11:10:27 -0300

    alteração caixa de textos

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 document.addEventListener("click",showcolor);
 
+document.getElementById("botao").addEventListener("click",showname)
+
 function showcolor()
 {
     document.body.style.backgroundColor= "red";
 }
-
+function showname()
+{
+    let usuario = document.getElementById("nome").value;
+    document.getElementById("mensagem").innerText = "Meu nome é" + usuario
+}
