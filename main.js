196483643b274bd16dcb0b217ea3138f94c0e24c
Caua-Kreca10 <129890305+Caua-Kreca10@users.noreply.github.com>
2023-04-05 11:12:11 -0300

    alteração text

--- main.js
+++ main.js
@@ -9,5 +9,5 @@ function showcolor()
 function showname()
 {
     let usuario = document.getElementById("nome").value;
-    document.getElementById("mensagem").innerText = "Meu nome é" + usuario
+    document.getElementById("mensagem").innerText = "Meu nome é" + usuario;
 }
